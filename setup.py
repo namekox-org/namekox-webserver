78d1343c6de91503725d2c9c5bac82ab7b5bfe6b
李满满 <limanman@corp.netease.com>
2021-02-10 16:13:32 +0800

    :underage:

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ from setuptools import find_packages, setup
 
 
 curdir = os.path.abspath(os.path.dirname(__file__))
-readme = open(os.path.join(curdir, 'README.md'), 'rb', 'utf-8').read()
+readme = open(os.path.join(curdir, 'READIT.md'), 'rb', 'utf-8').read()
 
 setup(
     name='namekox-webserver',
-    version='0.0.6',
+    version='0.0.7',
     description='namekox webserver',
     long_description_content_type='text/markdown',
     long_description=readme,
